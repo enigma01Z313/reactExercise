ad1f3e3f84d612b0feb2b107e078b82ac2d3f153
enigma01Z313 <f.ahmadyf94@gmail.com>
2022-06-18 21:56:19 +0430

    sync reducer for todo actions

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -2,8 +2,18 @@ export default function reducer(state, action) {
   const { type, payload } = action;
   console.log(type, payload, state);
 
-  if (type === "ADD_TODO")
-    return [...state, { id: new Date().getTime(), title: payload }];
+  if (type === "ADD_TODO") {
+    console.log(payload);
+    return [
+      ...state,
+      {
+        userId: 1,
+        id: payload.id,
+        title: payload.title,
+        completed: false,
+      },
+    ];
+  }
 
   if (type === "REMOVE_TODO")
     return state.filter((item) => item.id !== payload);
--- src/views/Index.jsx
+++ src/views/Index.jsx
@@ -2,6 +2,33 @@ import React, { useState, useEffect, useReducer } from "react";
 import todoReducer from "../reducers/todo";
 import InputText from "../componentsOld/inputs/Text";
 
+const asRemove = (id) =>
+  new Promise((resolve, reject) => {
+    fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        resolve();
+      });
+  });
+
+const asAdd = (title) =>
+  new Promise((resolve, reject) => {
+    fetch(`https://jsonplaceholder.typicode.com/todos/`, {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({title})
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        resolve(data);
+      });
+  });
+
 export default function Index() {
   const [task, setTask] = useState("");
   const [todos, todoDispatch] = useReducer(todoReducer, []);
@@ -10,18 +37,22 @@ export default function Index() {
     setTask(val);
   }
 
-  function addTodo(e) {
+  async function addTodo(e) {
     e.preventDefault();
-    todoDispatch({ type: "ADD_TODO", payload: task });
     setTask("");
+    const taskId = await asAdd(task);
+    todoDispatch({
+      type: "ADD_TODO",
+      payload: { id: taskId, title: task },
+    });
   }
 
-  function removeTodo(id) {
+  async function removeTodo(id) {
+    await asRemove(id);
     todoDispatch({ type: "REMOVE_TODO", payload: id });
   }
 
   useEffect(() => {
-    console.log("mounted");
     fetch("https://jsonplaceholder.typicode.com/todos/?_start=0&_limit=5")
       .then((res) => res.json())
       .then((data) => {
@@ -29,7 +60,6 @@ export default function Index() {
       });
   }, []);
 
-  console.log(todos);
   return (
     <>
       <form onSubmit={addTodo}>
